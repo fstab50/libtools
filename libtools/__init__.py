594a1fd281b7b7c89c785a9cdedb1b7fb1acde2b
Blake Huber <blakeca00@gmail.com>
2019-07-01 09:10:05 -0500

    __init__.py: updated with all library import deps

--- libtools/__init__.py
+++ libtools/__init__.py
@@ -1,5 +1,5 @@
 """
-Utilities Module - Scripts
+Tools & Utilities Module
 """
 
 from libtools.userinput import bool_convert, bool_assignment, ascii_lowercase
@@ -14,5 +14,55 @@ from libtools.progress import screen_dimensions, progress_meter
 from libtools.stdout import Colors, ColorMap
 
 # logging
-from libtools.logd import getLogger
-from libtools._version import __version__
+from libtools import logd
+from libtools._version import __version__ as version
+
+
+__author__ = 'Blake Huber'
+__version__ = version
+__credits__ = []
+__license__ = "GPL-3.0"
+__maintainer__ = "Blake Huber"
+__email__ = "blakeca00@gmail.com"
+__status__ = "Development"
+
+
+try:
+
+    # Linux
+    from libtools.oscodes_unix import exit_codes
+
+except Exception:
+    # windows
+    from libtools.oscodes_win import exit_codes
+
+
+## the following imports require __version__  ##
+
+from pyaws import logd
+
+PACKAGE = 'libtools'
+enable_logging = True
+log_mode = 'STREAM'          # log to cloudwatch logs
+log_filename = 'libtools.log'
+log_dir = os.getenv('HOME') + '/logs'
+log_path = log_dir + '/' + log_filename
+
+
+log_config = {
+    "PROJECT": {
+        "PACKAGE": PACKAGE,
+        "CONFIG_VERSION": __version__,
+    },
+    "LOGGING": {
+        "ENABLE_LOGGING": enable_logging,
+        "LOG_FILENAME": log_filename,
+        "LOG_DIR": log_dir,
+        "LOG_PATH": log_path,
+        "LOG_MODE": log_mode,
+        "SYSLOG_FILE": False
+    }
+}
+
+# shared, global logger object
+logger = logd.getLogger(__version__)
