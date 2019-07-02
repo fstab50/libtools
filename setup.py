d700cdd38e938f185836b56ac963c421f07467bf
Blake Huber <blakeca00@gmail.com>
2019-07-02 07:38:14 -0500

    setup.py: strip out unnecessary PostInstall classes, root user provisions, bash completion (not used)

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ requires = [
 
 _project = 'libtools'
 _root = os.path.abspath(os.path.dirname(__file__))
-_comp_fname = 'libtools-completion.bash'
 
 
 def _root_user():
@@ -102,114 +101,6 @@ class PostInstallDevelop(develop):
         develop.run(self)
 
 
-class PostInstall(install):
-    """
-    Summary.
-
-        Postinstall script to place bash completion artifacts
-        on local filesystem
-
-    """
-    def valid_os_shell(self):
-        """
-        Summary.
-
-            Validates install environment for Linux and Bash shell
-
-        Returns:
-            Success | Failure, TYPE bool
-
-        """
-        if platform.system() == 'Windows':
-            return False
-        elif which('bash'):
-            return True
-        elif 'bash' in subprocess.getoutput('echo $SHELL'):
-            return True
-        return False
-
-    def run(self):
-        """
-        Summary.
-
-            Executes post installation configuration only if correct
-            environment detected
-
-        """
-        if self.valid_os_shell():
-
-            completion_file = user_home() + '/.bash_completion'
-            completion_dir = user_home() + '/.bash_completion.d'
-            config_dir = user_home() + '/.config/' + _project
-
-            if not os.path.exists(os_parityPath(completion_file)):
-                create_artifact(os_parityPath(completion_file), 'file')
-            if not os.path.exists(os_parityPath(completion_dir)):
-                create_artifact(os_parityPath(completion_dir), 'dir')
-            if not os.path.exists(os_parityPath(config_dir)):
-                create_artifact(os_parityPath(config_dir), 'dir')
-
-            # ensure installation of home directory profile artifacts (data_files)
-            if not os.path.exists(os_parityPath(completion_dir + '/' + _comp_fname)):
-                copyfile(
-                    os_parityPath('bash' + '/' + _comp_fname),
-                    os_parityPath(completion_dir + '/' + _comp_fname)
-                )
-        install.run(self)
-
-
-class PostInstallRoot(install):
-    """
-    Summary.
-
-        Postinstall script to place bash completion artifacts
-        on local filesystem
-
-    """
-    def valid_os_shell(self):
-        """
-        Summary.
-
-            Validates install environment for Linux and Bash shell
-
-        Returns:
-            Success | Failure, TYPE bool
-
-        """
-        if platform.system() == 'Windows':
-            return False
-        elif which('bash'):
-            return True
-        elif 'bash' in subprocess.getoutput('echo $SHELL'):
-            return True
-        return False
-
-    def run(self):
-        """
-        Summary.
-
-            Executes post installation configuration only if correct
-            environment detected
-
-        """
-        # bash shell + root user
-        if self.valid_os_shell():
-
-            completion_dir = '/etc/bash_completion.d'
-            config_dir = module_dir() + _project + '/config'
-
-            if not os.path.exists(os_parityPath(config_dir)):
-                create_artifact(os_parityPath(config_dir), 'dir')
-
-            # ensure installation of home directory profile artifacts (data_files)
-            if not os.path.exists(os_parityPath(completion_dir + '/' + _comp_fname)):
-                copyfile(
-                    os_parityPath('bash' + '/' + _comp_fname),
-                    os_parityPath(completion_dir + '/' + _comp_fname)
-                )
-        install.run(self)
-
-
 def preclean(dst):
     if os.path.exists(dst):
         os.remove(dst)
@@ -247,82 +138,27 @@ def user_home():
         raise e
 
 
-# branch install based on user priviledge level
-
-if _root_user():
-
-    setup(
-        name=_project,
-        version=libtools.__version__,
-        description='Generic Tools & Utilities for processing i/o',
-        long_description=read('DESCRIPTION.rst'),
-        url='https://github.com/fstab50/libtools',
-        author=libtools.__author__,
-        author_email=libtools.__email__,
-        license='GPL-3.0',
-        classifiers=[
-            'Topic :: Software Development :: Build Tools',
-            'Development Status :: 4 - Beta',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Operating System :: POSIX :: Linux',
-        ],
-        keywords='code development tools',
-        packages=find_packages(exclude=['assets', 'docs', 'reports', 'scripts', 'tests']),
-        include_package_data=True,
-        install_requires=requires,
-        python_requires='>=3.6, <4',
-        cmdclass={
-            'install': PostInstallRoot
-        },
-        data_files=[
-            (os_parityPath('/etc/bash_completion.d'), ['bash/' + _comp_fname])
-        ],
-        entry_points={
-            'console_scripts': [
-                'libtools=libtools.cli:init_cli'
-            ]
-        },
-        zip_safe=False
-    )
-
-else:
-
-    # non-priviledged user
-
-    setup(
-        name=_project,
-        version=libtools.__version__,
-        description='Generic Tools & Utilities for processing i/o',
-        long_description=read('DESCRIPTION.rst'),
-        url='https://github.com/fstab50/libtools',
-        author=libtools.__author__,
-        author_email=libtools.__email__,
-        license='GPL-3.0',
-        classifiers=[
-            'Topic :: Software Development :: Build Tools',
-            'Development Status :: 4 - Beta',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Operating System :: POSIX :: Linux',
-        ],
-        keywords='code development tools',
-        packages=find_packages(exclude=['assets', 'docs', 'reports', 'scripts', 'tests']),
-        include_package_data=True,
-        install_requires=requires,
-        python_requires='>=3.6, <4',
-        cmdclass={
-            'install': PostInstall
-        },
-        data_files=[
-            (os_parityPath(user_home() + '/' + '.bash_completion.d'), ['bash/' + _comp_fname])
-        ],
-        entry_points={
-            'console_scripts': [
-                'libtools=libtools.cli:init_cli'
-            ]
-        },
-        zip_safe=False
-    )
+setup(
+    name=_project,
+    version=libtools.__version__,
+    description='Generic Tools & Utilities for processing i/o',
+    long_description=read('DESCRIPTION.rst'),
+    url='https://github.com/fstab50/libtools',
+    author=libtools.__author__,
+    author_email=libtools.__email__,
+    license='GPL-3.0',
+    classifiers=[
+        'Topic :: Software Development :: Build Tools',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+    ],
+    keywords='code development tools',
+    packages=find_packages(exclude=['assets', 'docs', 'reports', 'scripts', 'tests']),
+    include_package_data=True,
+    install_requires=requires,
+    python_requires='>=3.6, <4',
+    zip_safe=False
+)
