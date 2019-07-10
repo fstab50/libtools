a07ea37d7b2bc78c89ae2a669c09e396cce9297f
Blake Huber <blakeca00@gmail.com>
2019-07-10 06:29:05 -0500

    libtools/timer.py: end method updates to TimeDuration class; docstring updates

--- libtools/timer.py
+++ libtools/timer.py
@@ -7,7 +7,7 @@ Module Functions:
     - None
 
 Classes:
-    - TimeDuration
+    - TimeDuration: Measures duration
 
 """
 import time
@@ -20,7 +20,7 @@ class TimeDuration():
         Timer class; accuracy to 100th of a second (default, user changeable)
 
     Instantiation:
-        >>>  td = TimeDuration(accuracy=2)    # accuracy = decimal places; hundreds of a second (1.00)
+        >>>  td = TimeDuration(accuracy=2)
         >>>  s_time = td.start()
         >>>  e_time = td.end()
         >>>  print(e_time)
@@ -28,11 +28,23 @@ class TimeDuration():
         $ 31.29    #  seconds
     """
     def __init__(self, accuracy=2):
+        """
+        Args:
+            :accuracy (int): precision, number of decimal places.
+                Default is hundreds of a second (two decimal places, 1.00 seconds)
+        """
         self.start_time = None
         self.end_time = None
         self.precision = accuracy
 
     def start(self):
+        """
+        Summary:
+            Mark point in time where timer begins duration
+
+        Returns:
+            datetime representation; self.start_time stored internally as epoch seconds
+        """
         self.start_time = time.time()
         return self.__repr__()
 
@@ -40,8 +52,18 @@ class TimeDuration():
         return time.strftime('%H:%M:%S', time.localtime(self.start_time))
 
     def end(self):
+        """
+        Summary:
+            Mark point in time where timer ends duration
+
+        Returns:
+            elapsed time in seconds with precision, TYPE: int
+            Mark point datetime representation stored in self.end_time, TYPE: datetime
+        """
         try:
-            duration = time.time() - self.start_time
+            end_time = time.time()
+            duration = end_time - self.start_time
+            self.end_time = time.strftime('%H:%M:%S', time.localtime(end_time))
         except Exception as e:
             logger.exception(
                 '%s: Unknown error when calculating end time (Code: %s)' %
