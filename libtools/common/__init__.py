151dd85e8346541f21dd698530237b88360e94ff
Blake Huber <blakeca00@gmail.com>
2019-08-03 17:52:27 -0500

    import corrections with conversions

--- libtools/common/__init__.py
+++ libtools/common/__init__.py
@@ -2,4 +2,5 @@
 Common functionality to support modules
 """
 
-from libtools.facilities import os_parityPath, read_local_config, get_os
+from libtools.common.facilities import os_parityPath, read_local_config, get_os
+from libtools.common.environment import get_os, terminal_size
