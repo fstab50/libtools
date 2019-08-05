aae4d0fcc11d98e110422e42d162633b03f2750d
Blake Huber <blakeca00@gmail.com>
2019-08-05 15:28:19 -0500

    legacy backward support for export_json_object

--- libtools/io/__init__.py
+++ libtools/io/__init__.py
@@ -3,3 +3,6 @@ Input/ Ouput and Filesystem Operations
 """
 
 from libtools.io.binary import BinaryFile
+
+# legacy backward compatibility support
+from libtools.js import export_iterobject as export_json_object
--- libtools/js/__init__.py
+++ libtools/js/__init__.py
@@ -4,6 +4,3 @@ Javascript Obj Notation Operations
 
 from libtools.js.parse_json import ParameterSet
 from libtools.js.export import export_iterobject
-
-# legacy pyaws.utils support
-from libtools.js.export import export_iterobject as export_json_object
