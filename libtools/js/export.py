2850898ade237b99429ef6599dd276712ddf8bfa
Blake Huber <blakeca00@gmail.com>
2019-08-05 15:16:47 -0500

    libtools/js/export.py: added exception handling to export_iterobject module function to enable JSON serialization via string encoding datetime and other non-serializable objects

--- libtools/js/export.py
+++ libtools/js/export.py
@@ -5,14 +5,14 @@ from pygments import highlight, lexers, formatters
 from libtools import logger
 
 
-def export_iterobject(dict_obj, filename=None, logging=True):
+def export_iterobject(iterable, filename=None, logging=True):
     """
     Summary:
         pretty prints json, list, and tuple objects or exports iter
         object schema to filesystem object on the local filesystem
 
     Args:
-        :dict_obj (dict): dictionary object
+        :iterable (dict): dictionary, list, tuple, or complex object
         :filename (str):  name of file to be exported (optional)
 
     Returns:
@@ -32,42 +32,55 @@ def export_iterobject(dict_obj, filename=None, logging=True):
 
         if filename:
 
-            try:
+            with open(filename, 'w') as handle:
 
-                with open(filename, 'w') as handle:
-                    handle.write(json.dumps(dict_obj, indent=4, sort_keys=True))
+                try:
+
+                    handle.write(json.dumps(iterable, indent=4, sort_keys=True))
                     logger.info(
                         '%s: Wrote %s to local filesystem location' %
                         (inspect.stack()[0][3], filename))
-                handle.close()
 
-            except TypeError as e:
-                fx = inspect.stack()[0][3]
-                logger.warning('{}: object in dict not serializable: {}'.format(fx, str(e)))
-                raise e
+                except TypeError as e:
+                    # serialize via string json native string conversion
+                    handle.write(json.dumps(iterable, indent=4, sort_keys=True, default=str))
+
+            handle.close()
+
 
         elif is_tty():
+
             try:
 
                 # convert dict schema to json
-                json_str = json.dumps(dict_obj, indent=4, sort_keys=True)
+                json_str = json.dumps(iterable, indent=4, sort_keys=True)
 
-                print(
-                    highlight(
-                        json_str,
-                        lexers.JsonLexer(),
-                        formatters.TerminalFormatter()
-                    ).strip()
-                )
             except TypeError as e:
-                logger.info('element in json not serializable ({})'.format(e))
+                # serialize via string json native string conversion
+                json_str = json.dumps(iterable, indent=4, sort_keys=True, default=str)
+
+            print(
+                highlight(
+                    json_str,
+                    lexers.JsonLexer(),
+                    formatters.TerminalFormatter()
+                ).strip()
+            )
+
             if logging:
                 logger.info('{}: successful export to stdout'.format(inspect.stack()[0][3]))
             return True
 
         else:
-            # print output, but not to stdout; possibly commandline redirect
-            print(json.dumps(dict_obj, indent=4, sort_keys=True))
+
+            try:
+
+                # print output, but not to stdout; possibly commandline redirect
+                print(json.dumps(iterable, indent=4, sort_keys=True))
+
+            except TypeError as e:
+                # serialize via string json native string conversion
+                print(json.dumps(iterable, indent=4, sort_keys=True, default=str))
 
     except OSError as e:
         logger.critical(
