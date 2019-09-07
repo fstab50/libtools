012d0dc701c7fc347e6da7163cddca43a4376596
Blake Huber <blakeca00@gmail.com>
2019-09-07 11:22:10 -0500

    libtools/numfx.py added

---
+++ libtools/numfx.py
@@ -0,0 +1,31 @@
+"""
+Summary:
+    - Number manipulation functionality (float, int)
+    - Python3
+
+Module Functions:
+    - range_bind:  Returns an intger value between a floor and ceiling
+
+Classes:
+    - None
+
+"""
+import inspect
+from libtools import logger
+
+
+def range_bind(x, minx, maxx):
+    """
+    Bind an integer (x) to min, max range
+
+    Args:
+        :x (int): Value to bind to a min, max range
+        :minx (int): Minimum range (floor) value
+        :maxx (int): Maximum range (ceiling) value
+
+    Returns:
+        value in range(minx, maxx), TYPE: int
+
+    """
+    bind = lambda x, minx, maxx: max(min(maxx, round(x)), minx)
+    return bind(x, minx, maxx)
